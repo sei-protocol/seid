sei-protocol/sei-chain

Ezhu/staking dep gen mappings v2 rebase (#338)
* staking dynamic dependency generation mapping

* try staking messages

* update param name

* update config

* update msg distribution json in config

* fix linting error

* revert to basic msg type

loadtest/main.go
@@ -17,6 +17,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 	"github.com/sei-protocol/sei-chain/app"
 	dextypes "github.com/sei-protocol/sei-chain/x/dex/types"
 )
@@ -69,7 +70,7 @@ func run() {
 	fmt.Printf("%s - Finished\n", time.Now().Format("2006-01-02T15:04:05"))
 }
 
-func generateMessage(config Config, key cryptotypes.PrivKey, msgPerTx uint64) sdk.Msg {
+func generateMessage(config Config, key cryptotypes.PrivKey, msgPerTx uint64, validators []Validator) sdk.Msg {
 	var msg sdk.Msg
 	switch config.MessageType {
 	case "basic":
@@ -81,8 +82,34 @@ func generateMessage(config Config, key cryptotypes.PrivKey, msgPerTx uint64) sd
 				Amount: sdk.NewInt(1),
 			}),
 		}
+	case "staking":
+		msgType := config.MsgTypeDistr.SampleStakingMsgs()
+
+		switch msgType {
+		case "delegate":
+			msg = &stakingtypes.MsgDelegate{
+				DelegatorAddress: sdk.AccAddress(key.PubKey().Address()).String(),
+				ValidatorAddress: validators[rand.Intn(len(validators))].OpperatorAddr,
+				Amount:           sdk.Coin{Denom: "usei", Amount: sdk.NewInt(5)},
+			}
+		case "undelegate":
+			msg = &stakingtypes.MsgUndelegate{
+				DelegatorAddress: sdk.AccAddress(key.PubKey().Address()).String(),
+				ValidatorAddress: validators[rand.Intn(len(validators))].OpperatorAddr,
+				Amount:           sdk.Coin{Denom: "usei", Amount: sdk.NewInt(1)},
+			}
+		case "begin_redelegate":
+			msg = &stakingtypes.MsgBeginRedelegate{
+				DelegatorAddress:    sdk.AccAddress(key.PubKey().Address()).String(),
+				ValidatorSrcAddress: validators[rand.Intn(len(validators))].OpperatorAddr,
+				ValidatorDstAddress: validators[rand.Intn(len(validators))].OpperatorAddr,
+				Amount:              sdk.Coin{Denom: "usei", Amount: sdk.NewInt(1)},
+			}
+		default:
+			panic("Unknown message type")
+		}
 	case "dex":
-		msgType := config.MsgTypeDistr.Sample()
+		msgType := config.MsgTypeDistr.SampleDexMsgs()
 		orderPlacements := []*dextypes.Order{}
 		var orderType dextypes.OrderType
 		if msgType == "limit" {

sei-protocol/seid

sei-chain/loadtest/main.go
@@ -25,16 +25,7 @@ import (
 	dextypes "github.com/sei-protocol/sei-chain/x/dex/types"
 )
 
-var (
-	TestConfig         EncodingConfig
-	StakingQueryClient stakingtypes.QueryClient
-	//// Staking specific variables
-	Validators []stakingtypes.Validator
-	// DelegationMap is a map of delegator -> validator -> delegated amount
-	DelegationMap map[string]map[string]int
-	//// Tokenfactory specific variables
-	TokenFactoryDenomOwner map[string]string
-)
+var TestConfig EncodingConfig
 
 const (
 	VortexData = "{\"position_effect\":\"Open\",\"leverage\":\"1\"}"
@@ -83,7 +74,7 @@ func run() {
 	fmt.Printf("%s - Finished\n", time.Now().Format("2006-01-02T15:04:05"))
 }
 
-func generateMessage(config Config, key cryptotypes.PrivKey, msgPerTx uint64) (sdk.Msg, bool) {
+func (c *LoadTestClient) generateMessage(config Config, key cryptotypes.PrivKey, msgPerTx uint64) (sdk.Msg, bool) {
 	var msg sdk.Msg
 	messageTypes := strings.Split(config.MessageType, ",")
 	rand.Seed(time.Now().UnixNano())
@@ -115,23 +106,36 @@ func generateMessage(config Config, key cryptotypes.PrivKey, msgPerTx uint64) (s
 			Funds:        amount,
 		}
 	case Staking:
-		delegatorAddr := sdk.AccAddress(key.PubKey().Address()).String()
-		chosenValidator := Validators[rand.Intn(len(Validators))].OperatorAddress
-		// Randomly pick someone to redelegate / unbond from
-		srcAddr := ""
-		for k := range DelegationMap[delegatorAddr] {
-			if k == chosenValidator {
-				continue
+		msgType := config.MsgTypeDistr.SampleStakingMsgs()
+
+		switch msgType {
+		case "delegate":
+			msg = &stakingtypes.MsgDelegate{
+				DelegatorAddress: sdk.AccAddress(key.PubKey().Address()).String(),
+				ValidatorAddress: c.Validators[rand.Intn(len(c.Validators))].OperatorAddress,
+				Amount:           sdk.Coin{Denom: "usei", Amount: sdk.NewInt(5)},
 			}
-			srcAddr = k
-			break
+		case "undelegate":
+			msg = &stakingtypes.MsgUndelegate{
+				DelegatorAddress: sdk.AccAddress(key.PubKey().Address()).String(),
+				ValidatorAddress: c.Validators[rand.Intn(len(c.Validators))].OperatorAddress,
+				Amount:           sdk.Coin{Denom: "usei", Amount: sdk.NewInt(1)},
+			}
+		case "begin_redelegate":
+			msg = &stakingtypes.MsgBeginRedelegate{
+				DelegatorAddress:    sdk.AccAddress(key.PubKey().Address()).String(),
+				ValidatorSrcAddress: c.Validators[rand.Intn(len(c.Validators))].OperatorAddress,
+				ValidatorDstAddress: c.Validators[rand.Intn(len(c.Validators))].OperatorAddress,
+				Amount:              sdk.Coin{Denom: "usei", Amount: sdk.NewInt(1)},
+			}
+		default:
+			panic("Unknown message type")
 		}
-		msg = generateStakingMsg(delegatorAddr, chosenValidator, srcAddr)
 	case Tokenfactory:
 		denomCreatorAddr := sdk.AccAddress(key.PubKey().Address()).String()
 		// No denoms, let's mint
 		randNum := rand.Float64()
-		denom, ok := TokenFactoryDenomOwner[denomCreatorAddr]
+		denom, ok := c.TokenFactoryDenomOwner[denomCreatorAddr]
 		switch {
 		case !ok || randNum <= 0.33:
 			subDenom := fmt.Sprintf("tokenfactory-created-denom-%d", time.Now().UnixMilli())
@@ -140,7 +144,7 @@ func generateMessage(config Config, key cryptotypes.PrivKey, msgPerTx uint64) (s
 				Sender:   denomCreatorAddr,
 				Subdenom: subDenom,
 			}
-			TokenFactoryDenomOwner[denomCreatorAddr] = denom
+			c.TokenFactoryDenomOwner[denomCreatorAddr] = denom
 		case randNum <= 0.66:
 			msg = &tokenfactorytypes.MsgMint{
 				Sender: denomCreatorAddr,
@@ -231,7 +235,7 @@ func generateDexOrderPlacements(config Config, key cryptotypes.PrivKey, batchSiz
 	if config.MessageType == "failure_bank_malformed" {
 		orderType = -1
 	} else {
-		dexMsgType := config.MsgTypeDistr.Sample()
+		dexMsgType := config.MsgTypeDistr.SampleDexMsgs()
 		switch dexMsgType {
 		case Limit:
 			orderType = dextypes.OrderType_LIMIT
@@ -263,45 +267,6 @@ func generateDexOrderPlacements(config Config, key cryptotypes.PrivKey, batchSiz
 		})
 	}
 	return orderPlacements
-}
-
-func generateStakingMsg(delegatorAddr string, chosenValidator string, srcAddr string) sdk.Msg {
-	// Randomly unbond, redelegate or delegate
-	// However, if there are no delegations, do so first
-	var msg sdk.Msg
-	randNum := rand.Float64()
-	if _, ok := DelegationMap[delegatorAddr]; !ok || randNum <= 0.33 || srcAddr == "" {
-		msg = &stakingtypes.MsgDelegate{
-			DelegatorAddress: delegatorAddr,
-			ValidatorAddress: chosenValidator,
-			Amount:           sdk.Coin{Denom: "usei", Amount: sdk.NewInt(1)},
-		}
-		DelegationMap[delegatorAddr] = map[string]int{}
-		DelegationMap[delegatorAddr][chosenValidator] = 1
-	} else {
-
-		if randNum <= 0.66 {
-			msg = &stakingtypes.MsgBeginRedelegate{
-				DelegatorAddress:    delegatorAddr,
-				ValidatorSrcAddress: srcAddr,
-				ValidatorDstAddress: chosenValidator,
-				Amount:              sdk.Coin{Denom: "usei", Amount: sdk.NewInt(1)},
-			}
-			DelegationMap[delegatorAddr][chosenValidator]++
-		} else {
-			msg = &stakingtypes.MsgUndelegate{
-				DelegatorAddress: delegatorAddr,
-				ValidatorAddress: srcAddr,
-				Amount:           sdk.Coin{Denom: "usei", Amount: sdk.NewInt(1)},
-			}
-		}
-		// Update delegation map
-		DelegationMap[delegatorAddr][srcAddr]--
-		if DelegationMap[delegatorAddr][srcAddr] == 0 {
-			delete(DelegationMap, delegatorAddr)
-		}
-	}
-	return msg
 }
 
 func getLastHeight() int {